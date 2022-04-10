386be72dbf54b6434a1fb4da09fe436167e6ca49
Turbo <dev@turbo.ooo>
2022-04-10 03:23:16 -0500

    Add search endpoint

---
+++ src/pages/api/anime/search/index.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Fuse from 'fuse.js'
+import { getAllReleases, ReleaseList, ReleaseWithType, ShowWithTitle } from '../../../../utils/dbQueries'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ReleaseList | { id: string } | Fuse.FuseResult<{
+        show: ShowWithTitle;
+        releases: ReleaseWithType[];
+    }>[]>
+) {
+    switch (req.method) {
+    case 'GET': {
+        const releases = await getAllReleases()
+        const fuseOptions = {
+            keys: ['show.id', 'show.titles.title']
+        }
+        const fuse = new Fuse(releases, fuseOptions)
+        const results = fuse.search(req.query.q as string)
+        res.json(results)
+        break
+    }
+    default:
+        res.setHeader('Allow', 'GET')
+        res.status(405).end(`Method ${req.method} Nodt Allowed`)
+        break
+    }
+}
